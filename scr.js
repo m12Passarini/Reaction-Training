660df966e738530cc195f8bb0938368adea26709
“m12Passarini” <matheussouza.passarini@gmail.com>
2024-05-06 23:32:03 -0300

    changes to js

--- scr.js
+++ scr.js
@@ -1,13 +1,13 @@
 
-/* Objetos */
+/* Objects */
 const allTimeEl = document.querySelector("#alltime")
 const switchTimeEl = document.querySelector("#switchtime")
-const optionsEl = document.querySelector("#options")
 const initialEl = document.querySelector("#initial")
 const resetEl = document.querySelector("#reset")
-const inputsEl = document.querySelector(".inputs")
 const paragraphEl = document.querySelector("#paragraph")
-
+const inputsEl = document.querySelector(".inputs")
+const optionsEl = document.querySelector("#options")
+const displayEl = document.querySelector("#display")
 
 /*
   allTimeEl = o tempo total em que vai acontecer os eventos de troca
@@ -17,14 +17,18 @@ const paragraphEl = document.querySelector("#paragraph")
   resetEl = termina o evento
   inputsEl = Dados para a operação
   paragraphEl = texto de explicação
+  displayEl = lugar em que aparece o resultado
 */
 
+/* Functions */
 function start() {
 
   paragraphEl.style.display = "none"
   inputsEl.style.display = "none"
+  optionsEl.style.display = "none"
   initialEl.style.display = "none"
   resetEl.style.display = "flex"
+  displayEl.style.display = "flex"
 
   working(true)
 }
@@ -33,42 +37,55 @@ function end() {
 
   paragraphEl.style.display = "flex"
   inputsEl.style.display = "flex"
+  optionsEl.style.display = "flex"
   initialEl.style.display = "flex"
   resetEl.style.display = "none"
+  displayEl.style.display = "none"
 
   working(false)
 }
 
-optionsnumbers = [1, 2, 3, 4, 5, 6]  //* Input do optionsEl
-
-/* optionsnumbers = [] */
-
-/* while (optionsEl.value > 0) {
-  amount = optionsEl.value  -1
-  optionsnumbers.push(amount)
-}
- */
+let arrayal = []
 
 function working(condition) {
 
-  if (condition) {
-    interval = setInterval(() => {
-      const decision = Math.floor(Math.random() * (optionsnumbers.length + 1))
-      console.log(decision)
-    }, switchTimeEl.value * 1000); 
-  } else {
-    clearInterval(interval)
+  switch (optionsEl.value) {
+    case '2':
+      arrayal = [1, 2]
+    break; 
+    case '4':
+      arrayal = [1, 2, 3, 4]
+    break;
+    case '6': 
+      arrayal = [1, 2, 3, 4, 5, 6]
+    break;
   }
   
-    if(condition) {
+
+  if (condition) {
+    interval = setInterval(() => {
+      const decision = Math.floor(Math.random() * (arrayal.length + 1))
+      if (decision === 0) {
+        console.log(decision + 1)
+        displayEl.innerHTML = decision + 1
+      } else {
+        console.log(decision)
+        displayEl.innerHTML = decision
+      }
+    }, switchTimeEl.value * 1000);
+  
     setTimeout(() => {
       clearInterval(interval)
       end()
     }, allTimeEl.value * 1000); 
+
+  } else {
+
+    clearInterval(interval)
+
   }
-    
 }
   
-/* eventos */
+/* events */
 initialEl.addEventListener("click", start)
 resetEl.addEventListener("click", end)
